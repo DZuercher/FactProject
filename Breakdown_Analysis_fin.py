6a7bd15b796f7cc84858288a2c3ed34828dbaf93
DZuercher <dominikz@student.ethz.ch>
2017-10-08 15:18:06 +0200

    Improved Fitting and correct error calculations

--- Breakdown_Analysis_fin.py
+++ Breakdown_Analysis_fin.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[96]:
-
 import os
 import errno
 import math
@@ -12,10 +7,39 @@ import numpy as np
 from scipy.optimize import curve_fit
 from tqdm import tqdm
 
-def gauss_sum(x, *p):
+
+def gauss(x, coeffs):
+    return coeffs[0] * np.exp( -(x - coeffs[1])**2 / (2. * coeffs[2]**2))
+
+def gauss_int(coeffs):
+    xp = np.linspace(0, 1000, 1000)
+    int_sum = 0
+    for x in xp:
+        int_sum += gauss(x, coeffs)
+    return int_sum
+
+def gauss_int2(coeffs):
+    xp = np.linspace(0, 1000, 1000)
+    int_sum = 0
+    for x in xp:
+        int_sum += coeffs[0] * np.exp( -(x-coeffs[1])**2 / (2. * coeffs[2]**2) ) * (x - coeffs[1])**2 / (coeffs[2]**3)
+    return int_sum
+
+def gauss_int3(coeffs):
+    xp = np.linspace(0, 1000, 1000)
+    int_sum = 0
+    for x in xp:
+        int_sum += coeffs[0] * np.exp( -(x - coeffs[1])**2 / (2. * coeffs[2]**2)) * (x - coeffs[1]) / (coeffs[2]**2)
+    return int_sum
+
+def gauss_sump(x, *coeffs):
     '''Gaussian function used for fitting'''
-    A1, mu1, sigma1, A2, mu2, sigma2 = p
-    return A1*np.exp(-(x-mu1)**2/(2.*sigma1**2))+A2*np.exp(-(x-mu2)**2/(2.*sigma2**2))
+    return coeffs[0] * np.exp( -(x - coeffs[1])**2 / (2. * coeffs[2]**2)) + coeffs[3] * np.exp( -(x - coeffs[4])**2 / (2. * coeffs[5]**2))
+
+
+def gauss_sum(x, *coeffs):
+    '''Gaussian function used for fitting'''
+    return coeffs[0] * np.exp( -(x - coeffs[1])**2 / (2. * coeffs[2]**2)) + coeffs[3] * np.exp( -(x - coeffs[4])**2 / (2. * coeffs[5]**2)) + coeffs[6] * np.exp( -(x - coeffs[7])**2 / (2. * coeffs[8]**2))
 
 def replace_line(file_name, line_num, text):
     lines = open(file_name, 'r+').readlines()
@@ -48,9 +72,6 @@ def sipm_vs_t(f_sample, N, t_offset):
     # We truncate artefacts like negative amplitudes
     s_vs_t[s_vs_t < 0.0] = 0.0
     return s_vs_t
-
-
-# In[102]:
 
 def analyse(diode, point_num, voltage, temperature, debug_plotting):
     path = f"Data/Diode_{diode}/BV_{diode}_{point_num}.npy"
@@ -135,104 +156,136 @@ def analyse(diode, point_num, voltage, temperature, debug_plotting):
             
             
     plt.ion()
-    fig=plt.figure(1)
+    fig = plt.figure(1)
     n, bins, patches = plt.hist(
         integrals,
-        bins=200,
-        range=[0, 1000],
-        log=False,
-        histtype='step'
+        bins = 200,
+        range = [0, 1000],
+        log = False,
+        histtype = 'step'
     )
 
-    max1p=np.argmax(n)
-    min1p=np.argmin(n[max1p:max1p+30])+max1p
-    max2p=np.argmax(n[min1p:])+min1p
-    min2p=np.argmin(n[max2p:max2p+30])+max2p
+    max1p = np.argmax(n)
+    min1p = np.argmin(n[max1p:max1p + 30]) + max1p
+    max2p = np.argmax(n[min1p:]) + min1p
+    min2p = np.argmin(n[max2p:max2p + 30]) + max2p
 
-    max1=bins[max1p]
-    min1=bins[min1p]
-    max2=bins[max2p]
-    min2=bins[min2p]
+    max1 = bins[max1p]
+    min1 = bins[min1p]
+    max2 = bins[max2p]
+    min2 = bins[min2p]
 
-    bin_centres = (bins[:-1] + bins[1:])/2
-    fitdata = n[:min2p]
+    bin_centres = (bins[:-1] + bins[1:]) / 2
 
-    p0 = [n[max1p], max1, 10., n[max2p], max2, 10.]
+    #Fitting first and second peak
+    bin_centres = (bins[:-1] + bins[1:]) / 2
 
-    coeff = curve_fit(gauss_sum, bin_centres[:min2p], fitdata, p0=p0)[0]
+    p0t = [n[max1p], max1, 10., n[max2p], max2, 10.]
 
-    hist_fit = gauss_sum(bin_centres[:min2p], *coeff)
+    coefft, covt = curve_fit(gauss_sump, bin_centres[max1p - 20:min2p], n[max1p - 20:min2p], p0 = p0t)
 
-    norm1,mean1,std1,norm2,mean2,std2=coeff
-    cross_mean2=gauss_sum(mean2, *coeff)
-    cross_mean1=gauss_sum(mean1, *coeff)
-    cross_mean=cross_mean2 / cross_mean1
-    cross_err=cross_mean * (
-        abs(gauss_sum(mean1 + std1, *coeff) - cross_mean1) / cross_mean1 + 
-        abs(gauss_sum(mean2 + std2, *coeff) - cross_mean2) / cross_mean2
-    )
+    gain_est = coefft[4] - coefft[1]
+    gain_estp = int(gain_est * 200 / 1000)
 
 
-    plt.axvline(mean1,c='g',label="Gaussian Mean")
-    plt.axvline(mean2,c='g')
-    plt.axvline(bins[max1p],c='k',label="Init Max")
-    plt.axvline(bins[max2p],c='k')
-    plt.axvline(bins[min1p],c='y',label="Init Min")
-    plt.axvline(bins[min2p],c='y')
-    plt.plot(bin_centres[:min2p],hist_fit[:min2p],c='r')
+    p0 = [n[max1p], max1, 10., n[max2p], max2, 10., n[max2p + gain_estp], max2 + gain_est, 10.]
+
+    coeff, cov = curve_fit(gauss_sum, bin_centres[max1p - 20:], n[max1p - 20:], p0 = p0)
+
+    norm1, mean1, std1, norm2, mean2, std2, norm3, mean3, std3 = coeff
+
+    hist_fit = gauss_sum(bin_centres, *coeff)
+    single_fit1 = gauss(bin_centres, [norm1, mean1, std1])
+    single_fit2 = gauss(bin_centres, [norm2, mean2, std2])
+    single_fit3 = gauss(bin_centres, [norm3, mean3, std3])
+
+    cross_mean1 = gauss_int([norm1, mean1, std1])
+    cross_mean2 = gauss_int([norm2, mean2, std2])
+    
+    cross_mean1_err = np.sqrt(abs(gauss_int2([norm1, mean1, std1]))**2 * cov[2,2] +
+    abs(gauss_int3([norm1, mean1, std1]))**2 * cov[1,1] +
+    abs(cross_mean1 / norm1)**2 * cov[0,0]
+    	)
+
+    cross_mean2_err = np.sqrt(abs(gauss_int2([norm2, mean2, std2]))**2 * cov[5,5] +
+    abs(gauss_int3([norm2, mean2, std2]))**2 * cov[4,4] +
+    abs(cross_mean2 / norm2)**2 * cov[3,3]
+    	)
+
+    cross_mean = cross_mean2 / cross_mean1
+
+    cross_err = cross_mean * np.sqrt((cross_mean1_err / cross_mean1)**2 + (cross_mean2_err / cross_mean2)**2)
+
+    cross_err_alt = cross_mean * (
+                            abs(gauss_sum(mean1 + std1, *coeff) - cross_mean1) / cross_mean1 +
+                            abs(gauss_sum(mean2 + std2, *coeff) - cross_mean2) / cross_mean2
+                            )
+    gain = mean2 - mean1
+    #check error calculation / maybe also use cov from curve_fit
+    gain_err = np.sqrt(cov[1,1] + cov[4,4])
+    gain_err_alt = std1 / np.sqrt(cross_mean1) + std2 / np.sqrt(cross_mean2)
+
+    plt.axvline(mean1, c = 'g', label = "Gaussian Mean")
+    plt.axvline(mean2, c = 'g')
+    plt.axvline(bins[max1p], c = 'k', label = "Init Max")
+    plt.axvline(bins[max2p], c = 'k')
+    plt.axvline(bins[min1p], c = 'y', label = "Init Min")
+    plt.axvline(bins[min2p], c = 'y')
+    plt.axvline(bins[max2p + gain_estp], c = 'k')
+
+    plt.plot(bin_centres, hist_fit, c = 'r')
+    plt.plot(bin_centres, single_fit1, c = 'rosybrown', alpha = 0.3)
+    plt.plot(bin_centres, single_fit2, c = 'indianred', alpha = 0.3)
+    plt.plot(bin_centres, single_fit3, c = 'brown', alpha = 0.3)
     plt.xlabel("p.e.")
     plt.ylabel("# of events")
     plt.legend()
     plt.title(f"Vol: {voltage} V, Temp: {temperature}, Gain: {mean2-mean1}")
     plt.show()
 
+    print(f"1 Photon events: {cross_mean1}")
+    print(f"2 Photon events: {cross_mean2}")
+    print(f"Crosstalk Probability: {cross_mean*100} +/- {cross_err*100}%")
+    print(f"Gain: {gain} +/- {gain_err}")
 
-    print("1 Photon events: "+str(cross_mean1))
-    print("2 Photon events: "+str(cross_mean2))
-    print("Crosstalk Probability: "+str(cross_mean*100)+" +/-"+str(cross_err*100)+" % ")
-    print("Gain: "+str(mean2-mean1))
+
+    print(f"1 Photon events: {cross_mean1}")
+    print(f"2 Photon events: {cross_mean2}")
+    print(f"Crosstalk Probability: {cross_mean*100} +/- {cross_err*100}%")
+    print(f"Gain: {gain} +/- {gain_err}")
 
 
-    if os.path.isfile(f"Results/Diode_{diode}/BV/plotdata.txt")==False:
+    if os.path.isfile(f"Results/Diode_{diode}/BV/plotdata.txt") == False:
         file=open(f"Results/Diode_{diode}/BV/plotdata.txt","w+")
-        file.write("Voltage, Temperature, Gain \n")
+        file.write("Voltage, Temperature, Gain ,Gain err.\n")
         for i in range(100):
             file.write(". \n")
         file.close()
-    
-
-    try:
-        os.mkdir(f"Results/Diode_{diode}/BV/Point_{point_num}")
-    except OSError as exc:
-        if exc.errno != errno.EEXIST:
-            raise
-        pass
         
-    replace_line(f"Results/Diode_{diode}/BV/plotdata.txt",point_num,f"{voltage} {temperature} {mean2-mean1}")
+    replace_line(f"Results/Diode_{diode}/BV/plotdata.txt", point_num, f"{voltage} {temperature} {gain} {gain_err}")
 
-    fig.savefig(f"Results/Diode_{diode}/BV/Point_{point_num}/Fingerplot_BV_{diode}_{point_num}.png")
+    fig.savefig(f"Results/Diode_{diode}/BV/Fingerplot_BV_{diode}_{point_num}.png")
+    plt.close()
 
-
-# In[104]:
 
 #Adjust those !!!
-#Important: Delete plotdata.txt before running
 #----------------------------------------
 diode = 1
-point_num = [1,2,3,4]
-voltage = [69.3,69.5,69.7,69.9]
+point_num = [2, 3, 4, 5, 6, 8]
+voltage = [69.4972, 69.706, 69.8998, 70.1050, 70.2950, 70.0042]
 temperature = 25
 debug_plotting = False
 #----------------------------------------
+if os.path.isfile(f"Results/Diode_{diode}/BV/plotdata.txt") == True:
+    os.remove(f"Results/Diode_{diode}/BV/plotdata.txt")
 
-for i in range(4):
+for i in range(len(point_num)):
     analyse(diode, point_num[i], voltage[i], temperature, debug_plotting)
 
 
-# In[105]:
-
 volt = []
 gain = []
+err = []
 file = open(f"Results/Diode_{diode}/BV/plotdata.txt", 'r')
 for id, line in enumerate(file):
     if id != 0:
@@ -244,16 +297,18 @@ for id, line in enumerate(file):
             if i == 1:
                 temp = word
             if i == 2:
-                gain.append(float(word[:-1]))
+                gain.append(float(word))
+            if i == 3:
+                err.append(float(word[:-1]))
 file.close()
 
 def fitfunc(x,a,b):
-    return a+b*x
+    return a + b * x
 
-import scipy
+
 
 opt, cov = scipy.optimize.curve_fit(fitfunc, volt, gain)
-xp = np.linspace(69, 70, 100)
+xp = np.linspace(69, 71, 100)
 yp = opt[0] + opt[1] * xp
 bv = -opt[0] / opt[1]
 
@@ -261,20 +316,18 @@ err_a = np.sqrt(cov[0,0])
 err_b = np.sqrt(cov[1,1]) 
 err_bv = abs(bv * (err_a / opt[0] + err_b / opt[1]))
 
-fig2=plt.figure(2)
-plt.plot(xp,yp,'--')
-plt.plot(volt,gain,'.')
+fig2 = plt.figure(2)
+plt.plot(xp, yp, '--')
+plt.errorbar(x = volt, y = gain, yerr = err, fmt = '.')
 plt.title(f"Breakdown Voltage Diode {diode}: {bv:.3f} +/- {err_bv:.3f} V")
 plt.xlabel("Bias Voltage [V]")
 plt.ylabel("Gain")
 fig2.show()
 
 fig2.savefig(f"Results/Diode_{diode}/BV/Breakdown_{diode}.png")
+plt.close()
 
 print(f"Breakdown Voltage of Diode {diode} is: {bv:.3f} +/- {err_bv:.3f} V")
-
-
-# In[ ]:
 
 
 
